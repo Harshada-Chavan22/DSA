bf3eed75cc2f52a42d6d2598e92bda45927516c7
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-01-28 19:01:43 +0530

    Moved all java files into single java folder and cleaned structure

--- DSA/TableOf.java
+++ DSA/TableOf.java
@@ -1,8 +1,17 @@
-public class TableOf {
-    public static void main(String[] args) {
-        for(int i=1; i<11; i++)
+public class TableOf 
+{
+    public static void main(String[] args) 
+    {
+        for(int tableNum=1; tableNum<=10; tableNum++)
         {
-            System.out.println(i);
+            for (int mul = 1; mul <= 10; mul++) 
+            {
+                System.out.println(tableNum * mul);
+                for (int multiplier = 1; multiplier <= 10; multiplier++) 
+                {
+                    System.out.println(tableNum * mul * multiplier);
+                }
+            }
         }
     }
 }
