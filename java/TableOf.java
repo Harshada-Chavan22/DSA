6b1031aa2b1d3aae5f457a15f750369ef1aafc2e
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-01-29 09:14:20 +0530

    change fix

--- java/TableOf.java
+++ java/TableOf.java
@@ -4,13 +4,23 @@ public class TableOf
     {
         for(int tableNum=1; tableNum<=100; tableNum++)
         {
-            if(tableNum %2 ==0)
-            for (int mul = 1; mul <= 10; mul++) 
-            {
+                if(tableNum %2 ==0)
+                for (int mul = 1; mul <= 10; mul++) 
+               {
                 System.out.println(tableNum * mul);
+               }
+                // int secOut = tableNum*mul;
+                // if(secOut%2==0)
+                // {
+                //     for(int multiplier =1; multiplier<=10; multiplier++)
+                //     {
+                //         System.out.println(mul*multiplier);
+                //     }
+                // }
             }
+            
         }
     }
-}
+
 
 
