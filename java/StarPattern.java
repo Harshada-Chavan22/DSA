8b6e545292988b9ff0a7472d972e6446979d394e
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-01-31 08:22:01 +0530

    tried another way

--- java/StarPattern.java
+++ java/StarPattern.java
@@ -1,8 +1,34 @@
 public class StarPattern {
     public static void main(String[] args) {
+//         for (int i = 1; i <= 10; i++) {
+//     int limit = (i % 2 == 1) ? (i + 1) / 2 : 6 - (i / 2);
+
+//     for (int j = 1; j <= limit; j++) {
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+
+        // for(int i=1; i<=5; i++)
+        // {
+        //     for(int j=1; j<=5; j++)
+        //         System.out.print("*");
+        //         System.out.println();
+        // }
+        // System.out.println();
+        // for(int gsc = 1; gsc<=5 ; gsc++)
+        // {
+        //     int sc = (gsc<=5)? gsc : 6-gsc;
+        //     for(int i=1; i<=sc; i++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+            
+        // }
         for(int gsc = 1; gsc<=5 ; gsc++)
         {
-            for(int i=1; i<=gsc && i<=6-gsc; i++)
+            for(int i=1; i<=gsc || i<=6-gsc; i++)
             {
                 System.out.print("*");
             }
@@ -22,6 +48,7 @@ public class StarPattern {
         //     }
         //     System.out.println();
         // }
+
         // for(int gsc = 1, osc = 5; gsc<=5 && osc>=1; gsc++, osc--)
         // {
         //     for(int i=1; i<=gsc; i++)
