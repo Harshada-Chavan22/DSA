bc4fb18248f45f9fc8948eac1d7d9a6e8205b019
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-02-02 00:11:44 +0530

    solved more another patterns

--- java/NumberPattern.java
+++ java/NumberPattern.java
@@ -109,5 +109,19 @@ public class NumberPattern
 //             System.out.println();
 //         }
 
+ 
+
+        for (int i = 1; i <= 5; i++) {          
+            for (int j = 1; j <= 5; j++) {      
+
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // new line after each row
+        }
+
     }
 }
