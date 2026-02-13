665005c2527606f31adcf7568dc6c0dd7d4a7984
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-02-13 23:10:05 +0530

    multiple targets

--- java/BinarySearching.java
+++ java/BinarySearching.java
@@ -1,30 +1,34 @@
 public class BinarySearching {
     public static void main(String[] args) {
-        int [] arr = {2,3,5,9,14,16,18};
-        int target = 14;
-        int ans = binarySearch(arr, target);
+        int[] arr = {2,3,5,14,14,16,18};
+        int target = 5;
+        boolean ans = binarySearch(arr, target);
         System.out.println(ans);
     }
-    static int binarySearch(int [] arr, int target)
-    {
-        int starting = 0;
-        int ending = arr.length - 1;
 
-        while (starting <= ending) 
-        {
-           int halfPoint = starting + ending / 2;
+    static boolean binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
 
-           if (arr[halfPoint] < target) 
-           {
-            starting = halfPoint + 1;
-           }
-           else if(arr[halfPoint] < target)
-           {
-            ending = halfPoint - 1;
-           }
-           else
-            return halfPoint;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } 
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            } 
+            else {
+                // Found one occurrence
+                // Check safely on both sides
+                if ((mid > 0 && arr[mid - 1] == target) ||
+                    (mid < arr.length - 1 && arr[mid + 1] == target)) {
+                    return true;  // multiple found
+                }
+                return false; // only one occurrence
+            }
         }
-        return arr[ending];
+        return false; // not found
     }
 }
