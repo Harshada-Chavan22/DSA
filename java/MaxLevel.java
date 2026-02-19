dadf081313d19ea9b79dd982478ed851f92a4134
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-02-19 20:29:16 +0530

    MaxLevel

---
+++ java/MaxLevel.java
@@ -0,0 +1,19 @@
+public class MaxLevel {
+    public static void main(String[] args) {
+        int n = 10;
+    int ans = maxLevel(n);
+    System.out.println(ans);
+    }
+
+    static int maxLevel(int n)
+    {
+        int level = 0;
+        while (n >= level + 1) 
+        {
+           level++;
+           n = n - level; 
+        }
+        return level;
+    }
+}
+
