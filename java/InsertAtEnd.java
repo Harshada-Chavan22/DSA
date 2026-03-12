cdba2e1aa14b23d35ddba36d6e43eb5ad28c9244
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-03-12 23:16:10 +0530

    Insert at End

---
+++ java/InsertAtEnd.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.Node;
+
+public class InsertAtEnd {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        Node newNode = new Node(x);
+        if(head == null)
+    {
+        return newNode;
+    }
+        Node temp = head;
+        
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        
+        return head;
+    }
+}
