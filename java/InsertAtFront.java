48b9ade0855b66d09bdb940c6adf4ddfce5cb43d
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-03-12 23:17:04 +0530

    Insert at front

---
+++ java/InsertAtFront.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.Node;
+
+public class InsertAtFront {
+    public Node insertAtFront(Node head, int x) {
+        // code here
+        Node newNode = new Node(x);
+        newNode.next = head;
+        return newNode;
+    }
+}
