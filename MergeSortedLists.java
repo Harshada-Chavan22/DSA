8f922da2f84cb6d8d601941d5b64ee628942b15b
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-07-17 23:38:43 +0530

    merge two lists

---
+++ MergeSortedLists.java
@@ -0,0 +1,23 @@
+public class MergeSortedLists {
+    public static void main(String[] args) {
+        ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+            if(head1 == NULL || head2 == NULL)
+            {
+                return head1 == NULL ? head2 : head1;
+            }
+            //case 1:
+            if(head1-> val <= head2->val)
+            {
+                head1->next = mergeTwoLists(head1->next, head2);
+                return head1;
+            }
+            //case 2:
+            else
+            {
+                head2->next = mergeTwoLists(head1, head2->next);
+                return head2;
+            }
+        }
+    }
+
+}
