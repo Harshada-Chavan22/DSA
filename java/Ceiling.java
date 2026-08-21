08c75f163ae493e70cc1f060bc8680dd6c881bde
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-08-21 23:51:43 +0530

    info

--- java/Ceiling.java
+++ java/Ceiling.java
@@ -26,6 +26,7 @@ public class Ceiling {
             }
         }
         return letters[start % letters.length];
+        //the reason we are using modulo is because if the target is greater than the last element of the array then we will return the first element of the array.
     }
 }
 
